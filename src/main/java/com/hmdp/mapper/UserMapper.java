9a0117e6812ab848cb1576a0e4994f0120bb7d96
Hopefullymeet <1522512646@qq.com>
2025-04-12 10:46:47 +0800

    Updates codes for user control.

--- src/main/java/com/hmdp/mapper/UserMapper.java
+++ src/main/java/com/hmdp/mapper/UserMapper.java
@@ -2,6 +2,9 @@ package com.hmdp.mapper;
 
 import com.hmdp.entity.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +15,13 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @since 2021-12-22
  */
 public interface UserMapper extends BaseMapper<User> {
+
+    /**
+     * 根据手机号查询用户
+     * @param user
+     * @return
+     */
+    @Select("select * from tb_user where phone = #{phone}")
+    User selectByPhone(User user);
 
 }
--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,22 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -15,6 +27,78 @@ import org.springframework.stereotype.Service;
  * @since 2021-12-22
  */
 @Service
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Autowired
+    private UserMapper userMapper;
+
+    /**
+     * 发送验证码
+     * @param phone
+     * @param session
+     */
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        //1. 校验手机号
+        if(RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号不合法");
+        }
+
+        //2. 生成验证码
+        String code = RandomUtil.randomNumbers(6);
+
+        //3. 保存验证码
+        session.setAttribute("code", code);
+
+        //4. 发送验证码
+        log.info("短信验证码为：{}", code);
+
+        return Result.ok();
+    }
+
+    /**
+     *
+     * @param loginForm
+     * @param session
+     */
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        log.info("{}", loginForm);
+        if(RegexUtils.isPhoneInvalid(loginForm.getPhone())) {
+            return Result.fail("手机号不合法");
+        }
+
+        if(!loginForm.getCode().equals(session.getAttribute("code")) || session.getAttribute("code") == null) {
+            return Result.fail("验证码错误");
+        }
+
+        User toQueryUser = new User();
+        toQueryUser.setPhone(loginForm.getPhone());
+
+        User user = userMapper.selectByPhone(toQueryUser);
+
+//        //使用MyBatisPlus的方法
+//        User user = query().eq("phone", loginForm.getPhone()).one();
+
+        //若不存在，则创建
+        if(user == null) {
+            user = createUser(loginForm.getPhone());
+        }
+
+        //储存用户信息到session
+        session.setAttribute("user", user);
+
+        return Result.ok();
+    }
+
+    private User createUser(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(5));
+        user.setCreateTime(LocalDateTime.now());
+        user.setUpdateTime(LocalDateTime.now());
+
+        return user;
+    }
 }
